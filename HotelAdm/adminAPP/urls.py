5a138a65918a2ded40c2e1448974bc7d422d0115
SoyElDiego3 <127380387+Soyeldiego3@users.noreply.github.com>
2024-11-10 18:22:10 -0300

    mas hrefs

--- HotelAdm/adminAPP/urls.py
+++ HotelAdm/adminAPP/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('panel', views.panel, name='panel'),
+    path('panel', views.panel_clientes, name='clientes'),
+    path('usuarios', views.panel_usuarios, name='usuarios'),
+    path('hoteles', views.panel_hoteles, name='hoteles'),
+    path('clientes', views.panel_clientes, name='clientes'),
 ]
