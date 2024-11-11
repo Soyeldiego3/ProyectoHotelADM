9e79204302f1ebc6ff324703ef17ad4f04139c32
Karen Sofia Ortiz Silva <134814147+karen-sofia@users.noreply.github.com>
2024-11-11 00:10:20 -0300

    registroy menu hoteles y usuarios

--- HotelAdm/usuarioAPP/urls.py
+++ HotelAdm/usuarioAPP/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.menuUsuarios, name='menuUsuarios'),
+    path('', views.menuHoteles, name='menuHoteles'),
+    path('/menuUsuarios', views.registroUsuario, name='registroUsuario'),
+    path('/menuHoteles', views.registroHoteles, name='registroHoteles'),
+    
 ]
--- HotelAdm/usuarioAPP/views.py
+++ HotelAdm/usuarioAPP/views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'usuarioAPP/homepage/inicio.html')
 
-def cliente(request):
-    return render(request, 'clienteAPP/homepage/inicio.html')
+def menuHoteles(request):
+    return render (request,'clienteAPP/homepage/menuHoteles.html')
+
+def menuUsuarios(request):
+    return render (request,'clienteAPP/homepage/menuUsuarios.html')
+
+def registroUsuario(request):
+    return render(request, 'clienteAPP/homepage/registroUsuario.html')
+
+def registroHoteles(request):
+    return render (request,'clienteAPP/homepage/registroHoteles.html')
+
