fb1ddbcaa60b249084321f5914a81507291fb783
Karen Sofia Ortiz Silva <134814147+karen-sofia@users.noreply.github.com>
2024-11-11 00:31:12 -0300

    actualizacion urls

--- HotelAdm/usuarioAPP/urls.py
+++ HotelAdm/usuarioAPP/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.menuUsuarios, name='menuUsuarios'),
+    path('', views.menuHoteles, name='menuHoteles'),
+    path('/menuUsuarios', views.registroUsuarios, name='registroUsuario'),
+    path('/menuHoteles', views.registroHoteles, name='registroHoteles'),
 ]
